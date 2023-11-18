05fd644d429a557ea0678450f407d38400fbc4a7
matygullinruslan <matygullinruslan@gmail.com>
2023-11-18 23:59:20 +0300

    Исправил замечания

--- test_python_9_4.py
+++ test_python_9_4.py
@@ -94,6 +94,7 @@ def test_dicts():
     second = [1, 2, 3, 4, 5]
     # TODO создайте словарь
     d = dict(zip(second, first))
-    print(d)
+    d = dict(zip(first, second))
+    print(d.values())
     assert isinstance(d, dict)
     assert len(d) == 5
